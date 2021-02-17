d3c8660ccb48ad1c762db9ae36d3c5d23d5eb3f4
agjy <347217298@qq.com>
2021-02-18 01:46:47 +0800

    修改样式问题

--- src/main-dev.js
+++ src/main-dev.js
@@ -23,7 +23,7 @@ import 'nprogress/nprogress.css'
 
 import axios from 'axios'
 // 配置请求根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://gujuny.top:8888/api/private/v1/'
 // 在 request 拦截器中展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
   NProgress.start()
